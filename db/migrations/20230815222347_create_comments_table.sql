e7cf81ac0fe3cd5c527c3517fd2729472cd02e40
Kyle L <klomeli@gmail.com>
2023-08-16 11:38:42 -0400

    Removed unneeded tables.

--- db/migrations/20230815222347_create_comments_table.sql
+++ db/migrations/20230815222347_create_comments_table.sql
@@ -2,16 +2,7 @@
 CREATE TABLE public."comments" (
 	uuid uuid NOT NULL DEFAULT gen_random_uuid(),
 	"type" varchar NOT NULL,
-	-- message varchar NULL,
-	-- creation_timestamp timestamptz NOT NULL DEFAULT now(),
-	-- last_modified_timestamp timestamptz NOT NULL DEFAULT now(),
-	-- track_uuid uuid NULL,
-	-- pool_uuid uuid NULL,
-	-- discussion_uuid uuid NULL,
-	CONSTRAINT comments_pk PRIMARY KEY (uuid) --,
-	-- CONSTRAINT comments_fk FOREIGN KEY (pool_uuid) REFERENCES public.pools(uuid),
-	-- CONSTRAINT comments_fk_1 FOREIGN KEY (track_uuid) REFERENCES public.tracks(uuid),
-	-- CONSTRAINT comments_fk_2 FOREIGN KEY (discussion_uuid) REFERENCES public."comments"(uuid)
+	CONSTRAINT comments_pk PRIMARY KEY (uuid)
 );
 CREATE INDEX comments_type_idx ON public.comments USING btree (type);
 
